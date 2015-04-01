9add4fb635f8a85804afc9a1d470c032e470caaa
eivindgreibrokk <=>
2015-04-01 11:39:10 +0200

    supertux

--- bin/motd.sh
+++ bin/motd.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
 # Recommended applications
-echo "+--------------------------------+"
-echo "| gcompris        game           |"
-echo "| tuxmath         game           |"
-echo "| childsplay      game           |"
-echo "|                                |"
-echo "| viewnior        image viewer   |"
-echo "+--------------------------------+"
+echo "+--------------------------------------------+"
+echo "| gcompris        game                       |"
+echo "| tuxmath         Math shooting game         |"
+echo "| childsplay      game                       |"
+echo "| supertux        Super Mario game           |"
+echo "|                                            |"
+echo "| viewnior        image viewer               |"
+echo "+--------------------------------------------+"
+
+
+ 
+ 
+
